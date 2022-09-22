d7c6867acc4dbef07ad48351216a128a4fe256c1
Eric Li <e226li@uwaterloo.ca>
2022-09-22 19:12:16 +0100

    Update 'scripts/main.sh'

--- scripts/main.sh
+++ scripts/main.sh
@@ -5,4 +5,4 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get update -y
 apt-get upgrade -y
-apt-get install -y ca-certificates curl gnupg lsb-release nano wget tmux htop
+apt-get install -y ca-certificates curl gnupg lsb-release nano wget tmux htop python3 python3-pip
