224ffe4ac071e26a0b2872202c078b2e1a72851d
Eric Li <e226li@uwaterloo.ca>
2022-09-22 19:30:18 +0100

    Add 'scripts/wallabag/main.py'

---
+++ scripts/wallabag/main.py
@@ -0,0 +1,10 @@
+import os, sys
+
+with open("../nginx-site-template.conf", "r") as f:
+    template = f.read().replace("# server_name serverNameVar;", f"server_name {sys.argv[1]};")
+
+with open("/etc/nginx/sites-available/wallabag.conf", "w") as f:
+    f.write(template)
+    
+# depreceated, TODO: replace with os.subprocess
+os.system("../nginx-ensite.sh wallabag.conf")
