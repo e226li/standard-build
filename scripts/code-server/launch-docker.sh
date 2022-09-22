6e560d4b133ba5d9c26569fb8775afd8f86a8026
Eric Li <e226li@uwaterloo.ca>
2022-09-23 00:57:29 +0100

    Update 'scripts/code-server/launch-docker.sh'

--- scripts/code-server/launch-docker.sh
+++ scripts/code-server/launch-docker.sh
@@ -1,7 +1,7 @@
 docker run --net main_subnet --ip 172.30.3.1 \
   -e TZ=America/New_York \
-  -e HASHED_PASSWORD=$argon2i$v=19$m=4096,t=3,p=1$WD857amD54lMv03z9jZl3A$mP42PYPAmz3dwMV46r6RHf9h8l4FakxbSPO0MnWok7M \
-  -e SUDO_PASSWORD_HASH=$argon2i$v=19$m=4096,t=3,p=1$WD857amD54lMv03z9jZl3A$mP42PYPAmz3dwMV46r6RHf9h8l4FakxbSPO0MnWok7M \
+  -e HASHED_PASSWORD=$$argon2i$$v=19$$m=4096,t=3,p=1$$WD857amD54lMv03z9jZl3A$$mP42PYPAmz3dwMV46r6RHf9h8l4FakxbSPO0MnWok7M \
+  -e SUDO_PASSWORD_HASH=$$argon2i$$v=19$$m=4096,t=3,p=1$$WD857amD54lMv03z9jZl3A$$mP42PYPAmz3dwMV46r6RHf9h8l4FakxbSPO0MnWok7M \
   -v /opt/code-server/config:/config \
   --restart unless-stopped \
   lscr.io/linuxserver/code-server:latest
