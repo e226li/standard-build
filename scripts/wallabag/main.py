0f85af1a59321dd06b6570d41eb1351eaf93067a
Eric Li <e226li@uwaterloo.ca>
2022-09-22 19:32:12 +0100

    Update 'scripts/wallabag/main.py'

--- scripts/wallabag/main.py
+++ scripts/wallabag/main.py
@@ -1,7 +1,7 @@
 import os, sys
 
 with open("../nginx-site-template.conf", "r") as f:
-    template = f.read().replace("# server_name serverNameVar;", f"server_name {sys.argv[1]};")
+    template = f.read().replace("#serverNameVar", f"server_name {sys.argv[1]};")
 
 with open("/etc/nginx/sites-available/wallabag.conf", "w") as f:
     f.write(template)
