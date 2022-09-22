5b01af85ca6e43df70bc65416022a048f0693e5a
Eric Li <e226li@uwaterloo.ca>
2022-09-22 19:35:20 +0100

    Update 'scripts/wallabag/main.py'

--- scripts/wallabag/main.py
+++ scripts/wallabag/main.py
@@ -1,10 +1,11 @@
 import os, sys
 
 with open("../nginx-site-template.conf", "r") as f:
-    template = f.read().replace("#serverNameVar", f"server_name {sys.argv[1]};")
+    template = f.read().replace("#serverNameVar", f"{sys.argv[1]};").replace("#http://127.0.0.1:8080/", f"http://172.30.1.1:80/")
 
 with open("/etc/nginx/sites-available/wallabag.conf", "w") as f:
     f.write(template)
     
 # depreceated, TODO: replace with os.subprocess
-os.system("../nginx-ensite.sh wallabag.conf")
+os.system("sh ../nginx-ensite.sh wallabag.conf")
+os.system("sh launch-docker.sh")
