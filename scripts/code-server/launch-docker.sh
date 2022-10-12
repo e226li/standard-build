66238d6abaccee3a9b1422c301b1bf100d2a7d06
Eric Li <e226li@uwaterloo.ca>
2022-10-12 14:39:23 +0100

    Update 'scripts/code-server/launch-docker.sh'

--- scripts/code-server/launch-docker.sh
+++ scripts/code-server/launch-docker.sh
@@ -3,7 +3,7 @@ docker run --net main_subnet --ip 172.30.3.1 \
   -e PASSWORD=dummy \
   -e SUDO_PASSWORD=dummy \
   -e HASHED_PASSWORD=\$argon2i\$v=19\$m=4096,t=3,p=1\$WD857amD54lMv03z9jZl3A\$mP42PYPAmz3dwMV46r6RHf9h8l4FakxbSPO0MnWok7M \
-  -e SUDO_PASSWORD_HASH=\$argon2i\$v=19\$m=4096,t=3,p=1\$WD857amD54lMv03z9jZl3A\$mP42PYPAmz3dwMV46r6RHf9h8l4FakxbSPO0MnWok7M \
+  -e SUDO_PASSWORD_HASH='$argon2i$v=19$m=4096,t=3,p=1$WD857amD54lMv03z9jZl3A$mP42PYPAmz3dwMV46r6RHf9h8l4FakxbSPO0MnWok7M' \
   -v /opt/code-server/config:/config \
   --restart unless-stopped \
   lscr.io/linuxserver/code-server:latest
