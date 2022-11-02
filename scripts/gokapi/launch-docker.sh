29c224d61997609d43bd13616713044d889de85f
Eric Li <e226li@uwaterloo.ca>
2022-11-02 17:11:19 +0000

    Update 'scripts/gokapi/launch-docker.sh'

--- scripts/gokapi/launch-docker.sh
+++ scripts/gokapi/launch-docker.sh
@@ -1,5 +1,7 @@
 docker run -d --net main_subnet --ip 172.30.10.1 \
   --name=gokapi \
   -e TZ=America/New_York \
+  -v /opt/gokapi/config:/app/config \
+  -v /opt/gokapi/data:/app/data \
   --restart unless-stopped \
-  f0rc3/gokapi
+  f0rc3/gokapi:latest
