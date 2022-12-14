f5841112f1bd4335e0ab199ac9926423dae9139b
Eric Li <e226li@uwaterloo.ca>
2022-12-14 19:23:12 +0000

    Fix typo

--- scripts/selenium/launch-docker.sh
+++ scripts/selenium/launch-docker.sh
@@ -1,7 +1,6 @@
-docker run --net main_subnet --ip 172.30.13.1 \
+docker run -d --net main_subnet --ip 172.30.13.1 \
   -e TZ=America/New_York \
   --name=selenium \
-  -v /opt/changedetector/datastore:/datastore \
   --restart unless-stopped \
   --shm-size="2g" \
   selenium/standalone-chrome
