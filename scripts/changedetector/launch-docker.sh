a84915e2932761b29c462599726d721825337c5b
Eric Li <e226li@uwaterloo.ca>
2022-12-14 19:26:17 +0000

    Fix changedetector docker launch file to work with selenium

--- scripts/changedetector/launch-docker.sh
+++ scripts/changedetector/launch-docker.sh
@@ -2,5 +2,6 @@ docker run --net main_subnet --ip 172.30.12.1 \
   -e TZ=America/New_York \
   --name=changedetector \
   -v /opt/changedetector/datastore:/datastore \
+  -e WEBDRIVER_URL="http://172.30.13.1:4444/wd/hub" \
   --restart unless-stopped \
   dgtlmoon/changedetection.io
