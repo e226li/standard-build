71a5415a0c3e83c846f73c54df8b8c3738bf5acd
Eric Li <e226li@uwaterloo.ca>
2022-12-14 20:00:25 +0000

    Update 'main.sh'

--- main.sh
+++ main.sh
@@ -5,7 +5,7 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get update -y
 apt-get upgrade -y
-apt-get install -y ca-certificates curl gnupg lsb-release nano wget tmux htop python3 python3-pip git certbot
+apt-get install -y ca-certificates curl gnupg lsb-release nano wget tmux htop python3 python3-pip git certbot borgbackup
 
 rm -rf /etc/nginx/sites-available/default
 rm -rf /etc/nginx/sites-enabled/default
