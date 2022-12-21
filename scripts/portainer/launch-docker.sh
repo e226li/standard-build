2967d96198b5a7b7fe35f95fa546873e8b37e195
Eric Li <e226li@uwaterloo.ca>
2022-12-21 19:22:25 +0000

    Add 'scripts/portainer/launch-docker.sh'

---
+++ scripts/portainer/launch-docker.sh
@@ -0,0 +1,7 @@
+docker run --net main_subnet --ip 172.30.15.1 \
+  -e TZ=America/New_York \
+  --name=portainer \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /opt/portainer/data:/data \
+  --restart unless-stopped \
+  portainer/portainer-ce:latest
