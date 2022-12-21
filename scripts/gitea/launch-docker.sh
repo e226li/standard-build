82f211396845ec70cab4e72a262f35e2f61f9a94
Eric Li <e226li@uwaterloo.ca>
2022-12-21 20:08:17 +0000

    Add 'scripts/gitea/launch-docker.sh'

---
+++ scripts/gitea/launch-docker.sh
@@ -0,0 +1,9 @@
+docker run -d --net main_subnet --ip 172.30.16.1 \
+  -e TZ=America/New_York \
+  --name=gitea \
+  -v /opt/gitea/data:/data \
+  -v /etc/timezone:/etc/timezone:ro \
+  -v /etc/localtime:/etc/localtime:ro \
+  -p 1022:22
+  --restart unless-stopped \
+  portainer/portainer-ce:latest
