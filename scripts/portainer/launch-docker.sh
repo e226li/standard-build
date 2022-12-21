01575b6c4587b18c732eaadfef23bd715126d515
Eric Li <e226li@uwaterloo.ca>
2022-12-21 19:26:19 +0000

    Detach docker container

--- scripts/portainer/launch-docker.sh
+++ scripts/portainer/launch-docker.sh
@@ -1,4 +1,4 @@
-docker run --net main_subnet --ip 172.30.15.1 \
+docker run -d --net main_subnet --ip 172.30.15.1 \
   -e TZ=America/New_York \
   --name=portainer \
   -v /var/run/docker.sock:/var/run/docker.sock \
