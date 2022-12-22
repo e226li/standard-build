22034b08b51d35b1ddc0da9e285f74c003154339
Eric Li <e226li@uwaterloo.ca>
2022-12-22 18:21:20 +0000

    Add 'scripts/postgres/launch-docker.sh'

---
+++ scripts/postgres/launch-docker.sh
@@ -0,0 +1,9 @@
+. ./postgres.conf
+
+docker run -d --net main_subnet --ip 172.30.18.1 \
+  -e TZ=America/New_York \
+  --name=postgres \
+  -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
+  -v /opt/postgres/data:/var/lib/postgresql/data \
+  --restart unless-stopped \
+  postgres
