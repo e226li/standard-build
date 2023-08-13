bb9a7cf365346fe635ac594a55fbc25db0b516cb
Eric Li <e226li@uwaterloo.ca>
2023-08-13 21:02:35 +0100

    Update 'scripts/healthchecks/launch-docker.sh'

--- scripts/healthchecks/launch-docker.sh
+++ scripts/healthchecks/launch-docker.sh
@@ -1,5 +1,13 @@
+. ./email.conf
 docker run -d --net main_subnet --ip 172.30.23.1 \
   -e TZ=America/New_York \
+  -e DEBUG=False \
+  -e DEFAULT_FROM_EMAIL=DEFAULT_FROM_EMAIL \
+  -e EMAIL_HOST=EMAIL_HOST \
+  -e EMAIL_PORT=EMAIL_PORT \
+  -e EMAIL_HOST_USER=EMAIL_HOST_USER \
+  -e EMAIL_HOST_PASSWORD=EMAIL_HOST_PASSWORD \
+  -e EMAIL_USE_TLS=True \
   --name=healthchecks \
   --restart unless-stopped \
   lscr.io/linuxserver/healthchecks:latest
