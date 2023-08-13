80bb90813f3fe3e68cb2ff9fbd932aabf6878e35
Eric Li <e226li@uwaterloo.ca>
2023-08-13 21:17:15 +0100

    Fix missing $s

--- scripts/healthchecks/launch-docker.sh
+++ scripts/healthchecks/launch-docker.sh
@@ -2,11 +2,11 @@
 docker run -d --net main_subnet --ip 172.30.23.1 \
   -e TZ=America/New_York \
   -e DEBUG=False \
-  -e DEFAULT_FROM_EMAIL=DEFAULT_FROM_EMAIL \
-  -e EMAIL_HOST=EMAIL_HOST \
-  -e EMAIL_PORT=EMAIL_PORT \
-  -e EMAIL_HOST_USER=EMAIL_HOST_USER \
-  -e EMAIL_HOST_PASSWORD=EMAIL_HOST_PASSWORD \
+  -e DEFAULT_FROM_EMAIL=$DEFAULT_FROM_EMAIL \
+  -e EMAIL_HOST=$EMAIL_HOST \
+  -e EMAIL_PORT=$EMAIL_PORT \
+  -e EMAIL_HOST_USER=$EMAIL_HOST_USER \
+  -e EMAIL_HOST_PASSWORD=$EMAIL_HOST_PASSWORD \
   -e EMAIL_USE_TLS=True \
   --name=healthchecks \
   --restart unless-stopped \
