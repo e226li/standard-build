50dbd4966da8fdb60e3ac2caf036897615be1dac
Eric Li <e226li@uwaterloo.ca>
2023-08-14 23:13:31 +0100

    Add quick recovery

--- scripts/setup-template.py
+++ scripts/setup-template.py
@@ -8,6 +8,12 @@ parser.add_argument("domain")
 parser.add_argument("email")
 args = parser.parse_args()
 
+#QUICK_RECOVER
+os.makedirs(os.path.dirname("/opt/quick-recover/"), exist_ok=True)
+with open("/opt/quick-recover/#service.conf", "w") as f:
+    f.writeline(f"export DOMAIN='{parser.domain}'")
+    f.writeline(f"export EMAIL='{parser.email}'")
+
 #FILE_READ
 with open("../nginx-site-template.conf", "r") as f:
     template = f.read().replace("#serverNameVar", f"{args.domain}").replace("#http://127.0.0.1:8080/", f"#ip_address")
