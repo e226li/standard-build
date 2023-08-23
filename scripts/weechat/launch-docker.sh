a40da8d6b38ff7d877b2fe6d59bcdcd4f21973b9
Eric Li <e226li@uwaterloo.ca>
2023-08-23 19:32:55 +0100

    Add 'scripts/weechat/launch-docker.sh'

---
+++ scripts/weechat/launch-docker.sh
@@ -0,0 +1,6 @@
+docker run -dti --net main_subnet --ip 172.30.24.1 \
+  -e TZ=America/New_York \
+  -v /opt/weechat/config:/home/user/.weechat \
+  --name=weechat \
+  --restart unless-stopped \
+  weechat /home/user/.weechat
