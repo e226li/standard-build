301fc0d834267eb882fa29a7f5b7ec6f289a7a07
Eric Li <e226li@uwaterloo.ca>
2023-08-26 19:57:48 +0100

    Re-add git clone

--- setup.sh
+++ setup.sh
@@ -9,3 +9,5 @@ apt-get install -y ca-certificates curl gnupg lsb-release nano wget tmux htop py
 
 rm -rf /etc/nginx/sites-available/default
 rm -rf /etc/nginx/sites-enabled/default
+
+git clone https://git.162536.xyz/drunkendog/standard-build.git
