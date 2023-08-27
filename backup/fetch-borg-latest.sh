976fa7f2b7c6e8f4d5fbeee6812c247f797a5f3c
Eric Li <e226li@uwaterloo.ca>
2023-08-27 21:35:39 +0100

    Add user prompt to fetch-borg-latest

--- backup/fetch-borg-latest.sh
+++ backup/fetch-borg-latest.sh
@@ -2,5 +2,16 @@
 
 set -e
 
-wget -4 -O /usr/local/bin/borg-linux64 $(curl -s https://api.github.com/repos/borgbackup/borg/releases/latest| jq -r '.assets | .[] | select(.name == "borg-linux64") | .browser_download_url')
+download_version=curl -s https://api.github.com/repos/borgbackup/borg/releases/latest | jq -r '.name'
+download_url=curl -s https://api.github.com/repos/borgbackup/borg/releases/latest | jq -r '.assets | .[] | select(.name == "borg-linux64") | .browser_download_url'
+
+echo "Version: $download_version"
+echo "URL: $download_url"
+echo 'Continue (y/N)?'
+read answer
+if [ "$answer" == "${answer#[Yy]}" ] ;then 
+    exit 1
+fi
+
+wget -4 -O /usr/local/bin/borg-linux64 $download_url
 chmod -v +x /usr/local/bin/borg-linux64 
