0e43f8e5b8056551493164c4938fa375d2688f2a
Eric Li <e226li@uwaterloo.ca>
2023-12-20 03:33:37 +0000

    Add scripts/prometheus/launch-docker.sh

---
+++ scripts/prometheus/launch-docker.sh
@@ -0,0 +1,6 @@
+docker run -d --net main_subnet --ip 172.30.27.1 \
+  -v /opt/prometheus/prometheus.yml:/etc/prometheus/prometheus.yml \
+  -v /opt/prometheus/data:/prometheus \
+  --name=prometheus \
+  --restart unless-stopped \
+prom/prometheusdocker
