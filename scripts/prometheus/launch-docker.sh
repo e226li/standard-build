3a45de57d034006e7b58c91628bb2c6ec3707118
Eric Li <e226li@uwaterloo.ca>
2023-12-20 03:56:13 +0000

    Add auth to prometheus

--- scripts/prometheus/launch-docker.sh
+++ scripts/prometheus/launch-docker.sh
@@ -1,6 +1,7 @@
 docker run -d --net main_subnet --ip 172.30.27.1 \
   -v /opt/prometheus/prometheus.yml:/etc/prometheus/prometheus.yml \
+  -v /opt/prometheus/web.yml:/etc/prometheus/web.yml \
   -v /opt/prometheus/data:/prometheus \
   --name=prometheus \
   --restart unless-stopped \
-prom/prometheus
+prom/prometheus --config.file=/etc/prometheus/prometheus.yml --web.config.file=/etc/prometheus/web.yml
