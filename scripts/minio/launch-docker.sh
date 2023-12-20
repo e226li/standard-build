f69072cdedd297f1ef90bb8a2db7e264465e2f55
Eric Li <e226li@uwaterloo.ca>
2023-12-20 17:59:10 +0000

    Update scripts/minio/launch-docker.sh

--- scripts/minio/launch-docker.sh
+++ scripts/minio/launch-docker.sh
@@ -7,5 +7,7 @@ docker run -d --net main_subnet --ip 172.30.26.1 \
   --name=minio \
   -e "MINIO_ROOT_USER=$MINIO_USER" \
   -e "MINIO_ROOT_PASSWORD=$MINIO_PASSWORD" \
+  -e "MINIO_PROMETHEUS_URL=$PROMETHEUS_URL" \
+  -e "MINIO_PROMETHEUS_JOB_ID=$PROMETHEUS_JOB_ID" \
   --restart unless-stopped \
 quay.io/minio/minio server /data --console-address ":9090"
