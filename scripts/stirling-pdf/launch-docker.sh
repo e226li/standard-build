e8bf43f251d4581c4ae8764b2a38359e5c7e338f
Eric Li <e226li@uwaterloo.ca>
2024-01-26 17:40:38 -0500

    Add stirling-pdf to docker applications

---
+++ scripts/stirling-pdf/launch-docker.sh
@@ -0,0 +1,9 @@
+docker run -d --net main_subnet --ip 172.30.28.1 \
+  -v /opt/stirling-pdf/trainingData:/usr/share/tesseract-ocr/5/tessdata \
+  -v /opt/stirling-pdf/extraConfigs:/configs \
+  -v /opt/stirling-pdf/logs:/logs \
+  -v /opt/stirling-pdf/customFiles:/customFiles \
+  -e DOCKER_ENABLE_SECURITY=true \
+  -e SECURITY_ENABLE_LOGIN=true \
+  --name stirling-pdf \
+  frooodle/s-pdf:latest
